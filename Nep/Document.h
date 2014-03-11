18aa09bd2213a26ee47fa228210d5867fb4b4d42
Justin <jfarlow@mac.com>
2014-03-11 13:50:09 -0700

    Initial Commit

---
+++ Nep/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Nep
+//
+//  Created by Justin Farlow on 3/11/14.
+//  Copyright (c) 2014 UCSF. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+
+@end
---
+++ Nep/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Nep
+//
+//  Created by Justin Farlow on 3/11/14.
+//  Copyright (c) 2014 UCSF. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
